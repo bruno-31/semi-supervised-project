c42782294a813aa0eb937c13b03cfe764b30dbce
bruno <bruno.lecouat@gmail.com>
2017-10-18 00:54:00 +0800

    test collapse mode

--- ssl-gan/cifar_gan.py
+++ ssl-gan/cifar_gan.py
@@ -1,12 +1,7 @@
 import tensorflow as tf
-import nn
+import nn # OpenAI implemetation of weightnormalization (Salimans & Kingma)
 
 init_kernel = tf.random_normal_initializer(mean=0, stddev=0.05)
-
-
-def gaussian_noise_layer(input_layer, std):
-    noise = tf.random_normal(shape=tf.shape(input_layer), mean=0.0, stddev=std, dtype=tf.float32)
-    return input_layer + noise
 
 
 def leakyReLu(x, alpha=0.2, name=None):
@@ -26,50 +21,65 @@ def discriminator(inp, is_training, init=False):
     x = tf.reshape(inp, [-1,32,32,3])
     x = tf.layers.dropout(x,rate=0.2, training=is_training, name='dropout_0')
 
-    x = nn.conv2d(x, 96, nonlinearity=leakyReLu, init=init, counters=counter)
-    x = nn.conv2d(x, 96, nonlinearity=leakyReLu, init=init, counters=counter)
-    x = nn.conv2d(x, 96, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter)
-    # 16*16
+    # x = nn.conv2d(x, 96, nonlinearity=leakyReLu, init=init, counters=counter)
+    x = nn.conv2d(x, 96, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter) #=> 16*16
     x = tf.layers.dropout(x,rate=0.5, training=is_training, name='dropout_1')
 
-    x = nn.conv2d(x, 192, nonlinearity=leakyReLu, init=init, counters=counter)
-    x = nn.conv2d(x, 192, nonlinearity=leakyReLu, init=init, counters=counter)
-    x = nn.conv2d(x, 192, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter)
+    # x = nn.conv2d(x, 192, nonlinearity=leakyReLu, init=init, counters=counter)
+    x = nn.conv2d(x, 192, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter) # => 8*8
     # 8*8
     x = tf.layers.dropout(x,rate=0.5, training=is_training, name='dropout_2')
 
-    x = nn.conv2d(x, 192, padding='VALID', nonlinearity=leakyReLu, init=init, counters=counter)  # 5*5
-    x = nn.nin(x, 192, nonlinearity=leakyReLu,init=init,counters=counter)
-    x = nn.nin(x, 192, nonlinearity=leakyReLu,init=init,counters=counter)
+    x = nn.conv2d(x, 192, padding='VALID', nonlinearity=leakyReLu, init=init, counters=counter)  # 8*8
 
-    print(x)
-    intermediate_layer = x
 
-    x = tf.layers.average_pooling2d(x, pool_size=8, strides=1, name='avg_pool_0')
+    x = tf.layers.average_pooling2d(x, pool_size=8, strides=1, name='avg_pool_0') # batch *1*1* 192
     x = tf.squeeze(x, [1, 2])
 
-    logits = nn.dense(x, 10, nonlinearity=None, init=init, counters=counter)
+    intermediate_layer = x
+
+    logits = nn.dense(x, 1, nonlinearity=None, init=init, counters=counter)
 
     return logits, intermediate_layer
 
 
 def generator(batch_size, is_training, init):
+
     counter = {}
     z_seed = tf.random_uniform([batch_size, 100])
+    # z_seed = tf.random_normal([batch_size, 100])
 
-    x = nn.dense(z_seed, 4 * 4 * 512, init=init, counters=counter)
-    x = tf.layers.batch_normalization(x, training=is_training, name='batchnorm_1')
-    x = tf.nn.relu(x)
+    x = z_seed
+
+    with tf.variable_scope('dense_1'):
+        x = tf.layers.dense(x, units=4*4*512, kernel_initializer=tf.random_normal_initializer(stddev=0.05))
+    # x = nn.dense(z_seed, 4 * 4 * 512, init=init, counters=counter)
+        x = tf.layers.batch_normalization(x, training=is_training, name='batchnorm_1')
+        x = tf.nn.relu(x)
 
     x = tf.reshape(x, [-1, 4, 4, 512])
 
-    x = nn.deconv2d(x, 256,filter_size=[5,5], stride=[2,2],init=init, counters=counter)
-    x = tf.layers.batch_normalization(x, training=is_training, name='batchnorm_2')
-    x = tf.nn.relu(x)
+    # x = nn.deconv2d(x, 256,filter_size=[5,5], stride=[2,2],init=init, counters=counter) # [batch,256,8,8]
+    # x = tf.layers.batch_normalization(x, training=is_training, name='batchnorm_2')
+    # x = tf.nn.relu(x)
+    #
+    # x = nn.deconv2d(x, 128,filter_size=[5,5], stride=[2,2], init=init, counters=counter) # [batch,256,16,16]
+    # x = tf.layers.batch_normalization(x, training=is_training, name='batchnormn_3')
+    # x = tf.nn.relu(x)
+    with tf.variable_scope('deconv_1'):
+        x = tf.layers.conv2d_transpose(x, 256, [5, 5], strides=[2,2], padding='SAME', kernel_initializer=init_kernel)
+        x = tf.layers.batch_normalization(x, training=is_training, name='batchnorm_2')
+        x = tf.nn.relu(x)
 
-    x = nn.deconv2d(x, 128,filter_size=[5,5], stride=[2,2], init=init, counters=counter)
-    x = tf.layers.batch_normalization(x, training=is_training, name='batchnormn_3')
-    x = tf.nn.relu(x)
+    with tf.variable_scope('deconv_2'):
+        x = tf.layers.conv2d_transpose(x, 128, [5,5], strides=[2,2], padding='SAME', kernel_initializer=init_kernel)
+        x = tf.layers.batch_normalization(x, training=is_training, name='batchnormn_3')
+        x = tf.nn.relu(x)
 
-    output = nn.deconv2d(x, 3,filter_size=[5,5], stride=[2,2], nonlinearity=tf.nn.tanh, init=init, counters=counter)
+    # including weightnorm
+    with tf.variable_scope('deconv_3'):
+        output = nn.deconv2d(x, 3,filter_size=[5,5], stride=[2,2], nonlinearity=tf.nn.tanh, init=init, counters=counter)
+    # with tf.variable_scope('deconv3'):
+    #     output = tf.layers.conv2d_transpose(x, 3, [5, 5], strides=[2, 2], padding='SAME', activation=tf.tanh, kernel_initializer=init_kernel)
+    # [batch,32,32,3]
     return output
--- ssl-gan/train_gan_cifar.py
+++ ssl-gan/train_gan_cifar.py
@@ -1,24 +1,23 @@
 import os
 import time
 import sys
-
 import numpy as np
 import tensorflow as tf
-
 import cifar10_input
 import cifar_gan
+
 
 flags = tf.app.flags
 flags.DEFINE_integer("batch_size", 100, "batch size [128]")
 flags.DEFINE_string('data_dir', './data/cifar-10-python', 'data directory')
 flags.DEFINE_string('logdir', './log', 'log directory')
-flags.DEFINE_integer('seed', 1546, 'seed ')
-flags.DEFINE_integer('seed_data', 64, 'seed data')
-flags.DEFINE_integer('labeled', 1000, 'labeled data per class')
-flags.DEFINE_float('learning_rate', 0.003, 'learning_rate[0.003]')
-flags.DEFINE_integer('freq_print', 100, 'frequency image print tensorboard [20]')
+flags.DEFINE_integer('seed', 0, 'seed ')
+flags.DEFINE_integer('seed_data', 0, 'seed data')
+flags.DEFINE_integer('labeled', 400, 'labeled data per class')
+flags.DEFINE_float('learning_rate', 0.0003, 'learning_rate[0.003]')
+flags.DEFINE_integer('freq_print', 50, 'frequency image print tensorboard [20]')
 flags.DEFINE_float('unl_weight', 1.0, 'unlabeled weight [1.]')
-flags.DEFINE_float('lbl_weight', 1.0, 'unlabeled weight [1.]')
+flags.DEFINE_float('lbl_weight', 0.0, 'unlabeled weight [1.]')
 FLAGS = flags.FLAGS
 FLAGS._parse_flags()
 print("\nParameters:")
@@ -26,10 +25,11 @@ for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.lower(), value))
 print("")
 
+
 def display_progression_epoch(j, id_max):
     batch_progression = int((j / id_max) * 100)
     sys.stdout.write(str(batch_progression) + ' % epoch' + chr(13))
-    sys.stdout.flush
+    _ = sys.stdout.flush
 
 
 def main(_):
@@ -69,17 +69,19 @@ def main(_):
     unl = tf.placeholder(tf.float32, [FLAGS.batch_size, 32, 32, 3], name='unlabeled_data_input_pl')
     lbl = tf.placeholder(tf.int32, [FLAGS.batch_size], name='lbl_input_pl')
     is_training_pl = tf.placeholder(tf.bool, [], name='is_training_pl')
-
+    lr_pl = tf.placeholder(tf.float32, [], name='learning_rate_pl')
 
     gen = cifar_gan.generator
     dis = cifar_gan.discriminator
 
     with tf.variable_scope('generator_model') as scope:
         gen_init = gen(FLAGS.batch_size, is_training=is_training_pl, init=True)
-
-    with tf.variable_scope('generator_model', reuse=True):
+        [print(v.name) for v in tf.trainable_variables()]
+        print("")
+        scope.reuse_variables()
         gen_inp = gen(FLAGS.batch_size, is_training_pl, init=False)
-
+        [print(v.name) for v in tf.trainable_variables()]
+        print("")
     with tf.variable_scope('discriminator_model') as scope:
         dis(inp, is_training_pl, init=True)
         scope.reuse_variables()
@@ -93,39 +95,24 @@ def main(_):
         logits_test, _ = dis(inp, is_training_pl, False)
 
     with tf.name_scope('loss_functions'):
-        z_exp_lab = tf.reduce_mean(tf.reduce_logsumexp(logits_lab, axis=1))
-        rg = tf.cast(tf.range(0, FLAGS.batch_size), tf.int32)
-        idx = tf.stack([rg, lbl], axis=1)
-        l_lab = tf.gather_nd(logits_lab, idx)
-        loss_lab = -tf.reduce_mean(l_lab) + z_exp_lab
+        with tf.name_scope('generator_loss'):
+            loss_gen = tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_gen, labels=tf.ones_like(logits_gen)))
+        with tf.name_scope('discriminator_loss'):
+            d_loss_real = tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_unl, labels=tf.ones_like(logits_unl)))
+            d_loss_fake = tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_gen, labels=tf.zeros_like(logits_gen)))
+            loss_dis = d_loss_fake + d_loss_real
+        fool_rate = tf.reduce_mean(tf.cast(tf.greater(logits_gen,0),tf.float32))
+        accuracy_dis_unl = tf.reduce_mean(tf.cast(tf.greater(logits_unl,0),tf.float32))
+        accuracy_dis_gen = tf.reduce_mean(tf.cast(tf.less(logits_gen,0),tf.float32))
+        accuracy_dis= 0.5 * accuracy_dis_gen + 0.5 *accuracy_dis_unl
 
-        l_unl = tf.reduce_logsumexp(logits_unl, axis=1)
-        d = tf.reduce_mean(tf.nn.softplus(tf.reduce_logsumexp(logits_unl, axis=1)), axis=0) + \
-            tf.reduce_mean(tf.nn.softplus(tf.reduce_logsumexp(logits_gen, axis=1)), axis=0)
-        loss_unl = -0.5 * tf.reduce_mean(l_unl) + 0.5 * d
-
-        loss_dis = FLAGS.unl_weight * loss_unl + FLAGS.lbl_weight * loss_lab
-
-        accuracy_dis_unl = tf.reduce_mean(tf.cast(tf.greater(logits_unl, 0), tf.float32))
-        accuracy_dis_gen = tf.reduce_mean(tf.cast(tf.less(logits_gen, 0), tf.float32))
-        accuracy_dis = 0.5 * accuracy_dis_unl + 0.5 * accuracy_dis_gen
-
-        correct_pred = tf.equal(tf.cast(tf.argmax(logits_lab, 1), tf.int32), tf.cast(lbl, tf.int32))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-        correct_pred_test = tf.equal(tf.cast(tf.argmax(logits_test, 1), tf.int32), tf.cast(lbl, tf.int32))
-        accuracy_test = tf.reduce_mean(tf.cast(correct_pred_test, tf.float32))
-
-        # GENERATOR
-        m1 = tf.reduce_mean(layer_real, axis=0)
-        m2 = tf.reduce_mean(layer_fake, axis=0)
-        loss_gen = tf.reduce_mean(tf.square(m1 - m2))
-        fool_rate = tf.reduce_mean(tf.cast(tf.greater(logits_gen, 0), tf.float32))
 
     with tf.name_scope('optimizers'):
         # control op dependencies for batch norm and trainable variables
         tvars = tf.trainable_variables()
-        [print(var.name) for var in tvars]
 
         dvars = [var for var in tvars if 'discriminator_model' in var.name]
         gvars = [var for var in tvars if 'generator_model' in var.name]
@@ -135,8 +122,8 @@ def main(_):
         update_ops_gen = [x for x in update_ops if ('generator_model' in x.name)]
         update_ops_dis = [x for x in update_ops if ('discriminator_model' in x.name)]
 
-        optimizer_dis = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, beta1=0.5, name='dis_optimizer')
-        optimizer_gen = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, beta1=0.5, name='gen_optimizer')
+        optimizer_dis = tf.train.AdamOptimizer(learning_rate=lr_pl, beta1=0.5, name='dis_optimizer')
+        optimizer_gen = tf.train.AdamOptimizer(learning_rate=lr_pl, beta1=0.5, name='gen_optimizer')
 
         with tf.control_dependencies(update_ops_gen):
             train_gen_op = optimizer_gen.minimize(loss_gen, var_list=gvars)
@@ -145,47 +132,50 @@ def main(_):
         ema = tf.train.ExponentialMovingAverage(decay=0.9999)
         maintain_averages_op = ema.apply(dvars)
 
-        # with tf.control_dependencies(update_ops_dis):
-        with tf.control_dependencies([training_op]):
+        with tf.control_dependencies(update_ops_dis):
+            with tf.control_dependencies([training_op]):
                 train_dis_op = tf.group(maintain_averages_op)
 
         # copy_graph = [tf.assign(x,ema.average(y)) for x,y in zip(testvars,dvars)]
-        copy_graph = [tf.assign(x,y) for x,y in zip(testvars,dvars)]
+        copy_graph = [tf.assign(x, y) for x, y in zip(testvars, dvars)]
 
     with tf.name_scope('summary'):
         with tf.name_scope('dis_summary'):
-            tf.summary.scalar('loss_labeled', loss_lab, ['dis'])
-            tf.summary.scalar('loss_unlabeled', loss_unl, ['dis'])
-            tf.summary.scalar('classifier_accuracy', accuracy, ['dis'])
+            tf.summary.scalar('loss_unlabeled', loss_dis, ['dis'])
             tf.summary.scalar('discriminator_accuracy', accuracy_dis, ['dis'])
             tf.summary.scalar('discriminator_accuracy_fake_samples', accuracy_dis_gen, ['dis'])
             tf.summary.scalar('discriminator_accuracy_unl_samples', accuracy_dis_unl, ['dis'])
-            tf.summary.scalar('loss_dis',loss_dis,['dis'])
+            tf.summary.scalar('loss_discriminator', loss_dis, ['dis'])
+            tf.summary.scalar('learning_rate', lr_pl, ['dis'])
 
         with tf.name_scope('gen_summary'):
             tf.summary.scalar('loss_generator', loss_gen, ['gen'])
             tf.summary.scalar('fool_rate', fool_rate, ['gen'])
 
         with tf.name_scope('image_summary'):
-            tf.summary.image('gen_digits', tf.reshape(gen_inp, [-1, 28, 28, 1]), 20, ['image'])
+            tf.summary.image('gen_digits', gen_inp, 20, ['image'])
+            tf.summary.image('input_images', inp, 10, ['image'])
 
         sum_op_dis = tf.summary.merge_all('dis')
         sum_op_gen = tf.summary.merge_all('gen')
         sum_op_im = tf.summary.merge_all('image')
 
+    init_gen= [var.initializer for var in gvars][:-3]
+    [print(x.name) for x in init_gen]
     '''//////perform training //////'''
     print('start training')
     with tf.Session() as sess:
+        sess.run(init_gen)
         init = tf.global_variables_initializer()
-        #Data-Dependent Initialization of Parameters as discussed in DP Kingma and Salimans Paper
+        # Data-Dependent Initialization of Parameters as discussed in DP Kingma and Salimans Paper
         sess.run(init, feed_dict={inp: trainx_unl[0:FLAGS.batch_size], is_training_pl: True})
         print('initialization done')
-
+        writer = tf.summary.FileWriter(FLAGS.logdir, sess.graph)
         train_batch = 0
 
         for epoch in range(200):
             begin = time.time()
-            train_loss_lab, train_loss_unl, train_loss_gen, train_acc, test_acc = [0, 0, 0, 0, 0]
+            train_loss_dis, train_loss_unl, train_loss_gen, train_acc, test_acc = [0, 0, 0, 0, 0]
 
             # construct randomly permuted minibatches
             trainx = []
@@ -199,6 +189,7 @@ def main(_):
             trainx_unl = trainx_unl[rng.permutation(trainx_unl.shape[0])]  # shuffling unl dataset
             trainx_unl2 = trainx_unl2[rng.permutation(trainx_unl2.shape[0])]
 
+            lr = min(1., 3 - epoch / 400) * FLAGS.learning_rate # decayed learning rate feed to lr_pl
             # training
             for t in range(nr_batches_train):
                 display_progression_epoch(t, nr_batches_train)
@@ -209,37 +200,32 @@ def main(_):
                 feed_dict = {inp: trainx[ran_from:ran_to],
                              lbl: trainy[ran_from:ran_to],
                              unl: trainx_unl[ran_from:ran_to],
-                             is_training_pl: False}
-                _, ll, lu, acc = sess.run([train_dis_op, loss_lab, loss_unl, accuracy],
+                             is_training_pl: False,
+                             lr_pl: lr}
+                _, ld, sm = sess.run([train_dis_op, loss_dis, sum_op_dis],
                                               feed_dict=feed_dict)
-                train_loss_lab += ll
-                train_loss_unl += lu
-                train_acc += acc
+                train_loss_dis += ld
+                writer.add_summary(sm, train_batch)
 
                 # train generator
-                _, lg = sess.run([train_gen_op, loss_gen], feed_dict={unl: trainx_unl2[ran_from:ran_to],
-                                                                                      is_training_pl: True})
+                _, lg, sm = sess.run([train_gen_op, loss_gen, sum_op_gen], feed_dict={unl: trainx_unl2[ran_from:ran_to],
+                                                                                      is_training_pl: True, lr_pl: lr})
                 train_loss_gen += lg
                 train_batch += 1
+                writer.add_summary(sm, train_batch)
 
-            train_loss_lab /= nr_batches_train
-            train_loss_unl /= nr_batches_train
-            train_acc /= nr_batches_train
+                if t % FLAGS.freq_print == 0:
+                    x = np.random.randint(0, 4000)
+                    sm = sess.run(sum_op_im,
+                                  feed_dict={is_training_pl: True, inp: trainx_unl[x:x + FLAGS.batch_size], lr_pl: lr})
+                    writer.add_summary(sm, train_batch)
 
-            # Testing
-            sess.run(copy_graph)
-            for t in range(nr_batches_test):
-                ran_from = t * FLAGS.batch_size
-                ran_to = (t + 1) * FLAGS.batch_size
-                feed_dict = {inp: testx[ran_from:ran_to],
-                             lbl: testy[ran_from:ran_to],
-                             is_training_pl: False}
-                test_acc += sess.run(accuracy_test, feed_dict=feed_dict)
-            test_acc /= nr_batches_test
+            train_loss_dis /= nr_batches_train
+            train_loss_gen /= nr_batches_train
 
-            print("Epoch %d--Time = %ds | loss gen = %.4f | loss lab = %.4f | loss unl = %.4f "
-                  "| train acc = %.4f| test acc = %.4f"
-                  % (epoch, time.time() - begin, train_loss_gen, train_loss_lab, train_loss_unl, train_acc, test_acc))
+
+            print("Epoch %d--time = %ds--lr = %.1f | loss gen = %.4f | loss dis = %.4f |"
+                  % (epoch, time.time() - begin,lr, train_loss_gen, train_loss_dis))
 
 
 if __name__ == '__main__':
