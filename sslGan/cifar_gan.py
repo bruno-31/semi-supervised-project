be3c12ed874e19df2acc0051b6ab1a42d308da0d
bruno <bruno.lecouat@gmail.com>
2017-10-19 23:54:21 +0800

    working version openAI heavy

--- sslGan/cifar_gan.py
+++ sslGan/cifar_gan.py
@@ -22,14 +22,16 @@ def discriminator(inp, is_training, init=False):
     x = tf.layers.dropout(x, rate=0.2, training=is_training, name='dropout_0')
 
     x = nn.conv2d(x, 32, nonlinearity=leakyReLu, init=init, counters=counter)
+    x = nn.conv2d(x, 32, nonlinearity=leakyReLu, init=init, counters=counter)
     x = nn.conv2d(x, 32, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter)  # => 16*16
     x = tf.layers.dropout(x, rate=0.2, training=is_training, name='dropout_1')
 
     x = nn.conv2d(x, 64, nonlinearity=leakyReLu, init=init, counters=counter)
+    x = nn.conv2d(x, 32, nonlinearity=leakyReLu, init=init, counters=counter)
     x = nn.conv2d(x, 64, stride=[2, 2], nonlinearity=leakyReLu, init=init, counters=counter)  # => 8*8
     x = tf.layers.dropout(x, rate=0.2, training=is_training, name='dropout_2')
 
-    x = nn.conv2d(x, 128, padding='VALID', nonlinearity=leakyReLu, init=init, counters=counter)  # 8*8
+    x = nn.conv2d(x, 128, padding='SAME', nonlinearity=leakyReLu, init=init, counters=counter)  # 8*8
     x = nn.conv2d(x, 128, padding='SAME', nonlinearity=leakyReLu, init=init, counters=counter)  # 8*8
 
     x = tf.layers.max_pooling2d(x, pool_size=8, strides=1, name='avg_pool_0')  # batch *1*1* 192
@@ -44,8 +46,6 @@ def discriminator(inp, is_training, init=False):
 
 def generator(z_seed, is_training, init):
     counter = {}
-    # z_seed = tf.random_uniform([batch_size, 100])
-    # z_seed = tf.random_normal([batch_size, 100])
     x = z_seed
     with tf.variable_scope('dense_1'):
         x = tf.layers.dense(x, units=4 * 4 * 512, kernel_initializer=tf.random_normal_initializer(stddev=0.05))
